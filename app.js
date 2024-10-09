606a9ff3084cca3d7655a048cddbaa1b0e9097d6
AK1239 <akilkhatri235@gmail.com>
2024-10-09 10:20:20 +0300

    boilerplate code

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('04 Store API')
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="api/v1/products">products route</a>');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    //connectDB
+    app.listen(port, console.log(`Server is listening on ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
