1b545a27c8c24fb00175a38d2fb4783d806e1b94
AK1239 <akilkhatri235@gmail.com>
2024-10-11 14:52:10 +0300

    updated controller file

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,19 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ msg: "Product Testing Route" });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "Product Route" });
+  const { featured } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
