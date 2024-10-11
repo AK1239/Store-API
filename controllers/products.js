4314f690fd9e15f1f0637b153e439f1b11309fc3
AK1239 <akilkhatri235@gmail.com>
2024-10-11 15:28:25 +0300

    pagination

--- controllers/products.js
+++ controllers/products.js
@@ -5,13 +5,12 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
-
   if (company) {
     queryObject.company = company;
   }
@@ -28,6 +27,16 @@ const getAllProducts = async (req, res) => {
     result = result.sort(sortList);
   }
 
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
